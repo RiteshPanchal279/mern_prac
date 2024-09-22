62bdd11ce671d6c36b3ea9fb427b4a85f4d51b04
Ritesh Panchal <141012901+RiteshPanchal279@users.noreply.github.com>
2024-09-22 16:25:49 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -26,7 +26,7 @@ const Login = () => {
       return handleError("Email and Password is required");
     }
     try {
-      const url = "http://localhost:8080/auth/login";
+      const url = "https://loginprac-7js1inhmg-ritesh-panchals-projects.vercel.app/auth/login";
       const responce = await fetch(url, {
         method: "POST",
         headers: {
